94b1e4f9fcfcba064a288a4660b3e32ad1fce190
mahyarmadad <erfansam8891@yahoo.com>
2022-07-29 15:13:37 +0430

    add theme to document

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -2,11 +2,19 @@ import * as React from "react";
 import Document, {Html, Head, Main, NextScript} from "next/document";
 import createEmotionServer from "@emotion/server/create-instance";
 import createEmotionCache from "@Functions/emotionCache";
+import theme from "../styles/theme";
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <meta name="theme-color" content={theme.palette.primary.main} />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
